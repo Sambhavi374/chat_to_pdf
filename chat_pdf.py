12cec1559fc9eafefeaa447ca670b5219e38ac61
Sambhavi Sharma <149924866+Sambhavi374@users.noreply.github.com>
2025-04-18 13:19:51 +0000

    qdrant db connection remain

--- chat_pdf.py
+++ chat_pdf.py
@@ -1,6 +1,13 @@
+from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+
+
+import getpass
+import os
 
 pdf_path = Path(__file__).parent / "Python_Handbook.pdf"
 loader = PyPDFLoader(pdf_path)
@@ -13,5 +20,33 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 split_docs = text_splitter.split_documents(documents) ## splits the documents into smaller chunks
 
-embeddings = OpenAIEmbeddings()
 
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("AIzaSyA6bU0-anJWB8ZKb-kNAR8ebYEentwvoGU")
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/text-embedding-004"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents = [],
+    url = "http://localhost:6333",
+    collection_name = "pdf",
+    embedding = embeddings,
+)
+
+vector_store.add_documents(split_docs) ## adds the split documents to the vector store
+print("completed data ingestion")
+
+retriver = QdrantVectorStore.from_existing_collection(
+    url = "http://localhost:6333",
+    collection_name = "pdf",
+    embedding = embeddings,
+)
+
+search_results = retriver.similarity_search(
+    query = "What is the purpose of the __init__ method in Python?", 
+    embedding = embeddings
+)
+
+print("Search results:", search_results)
